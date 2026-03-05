ba1c48a61a5215960173e2bf942a5ac856bbda74
KatherineDG <katherinedomeqg@gmail.com>
2026-03-05 19:32:19 -0300

    feat: Loading view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Profile } from './pages/profile/profile';
+import { Loading } from './pages/loading/loading';
 
 export const routes: Routes = [
-    { path: 'profile', component: Profile}
+    { path: '', component: Loading },
+    { path: 'profile', component: Profile }
 ];
