06655f25f83fc138411e327f0adbe7d1d6eb3407
KatherineDG <katherinedomeqg@gmail.com>
2026-03-05 15:10:08 -0300

    feat: Alt for links and Hover bottom menu

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -9,16 +9,16 @@ import { Component } from '@angular/core';
 export class Menu {
   //Poner alt y href
   internalLinks = [
-    { iconCDN: "/icons/icons8-user-96.png", title: "Perfil"},
-    { iconCDN: "/icons/icons8-shine-96.png", title: "Experiencia"},
-    { iconCDN: "/icons/icons8-stack-96.png", title: "Stack"},
-    { iconCDN: "/icons/icons8-launch-96.png", title: "Proyectos"},
-    { iconCDN: "/icons/icons8-graduation-cap-96.png", title: "Educación"}
+    { iconCDN: "/icons/icons8-user-96.png", title: "Perfil", alt: "Icon User"},
+    { iconCDN: "/icons/icons8-shine-96.png", title: "Experiencia", alt: "Icon Shine"},
+    { iconCDN: "/icons/icons8-stack-96.png", title: "Stack", alt: "Icon Stack"},
+    { iconCDN: "/icons/icons8-launch-96.png", title: "Proyectos", alt: "Icon Launch"},
+    { iconCDN: "/icons/icons8-graduation-cap-96.png", title: "Educación", alt: "Icon Graduation Cap"}
   ]
 
   externalLinks = [
-    { iconCDN: "/icons/icons8-linkedin-96.png", title: "Linkedin"},
-    { iconCDN: "/icons/icons8-github-logo-96.png", title: "GitHub"},
-    { iconCDN: "/icons/icons8-gmail-96.png", title: "Contacto"}
+    { iconCDN: "/icons/icons8-linkedin-96.png", title: "Linkedin", alt: "Icon Linkedin"},
+    { iconCDN: "/icons/icons8-github-logo-96.png", title: "GitHub", alt: "Icon GitHub"},
+    { iconCDN: "/icons/icons8-gmail-96.png", title: "Contacto", alt: "Icon Gmail"}
   ]
 }
