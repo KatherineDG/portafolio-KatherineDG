3c122f462c7f5880f4b8dca0379fb04ee5fd6b17
KatherineDG <katherinedomeqg@gmail.com>
2026-03-05 12:56:02 -0300

    feat:Bottom Menu

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -7,11 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './menu.css',
 })
 export class Menu {
-  linkeds = [
+  internalLinks = [
     { iconCDN: "/icons/icons8-user-96.png", title: "Perfil"},
     { iconCDN: "/icons/icons8-shine-96.png", title: "Experiencia"},
     { iconCDN: "/icons/icons8-stack-96.png", title: "Stack"},
     { iconCDN: "/icons/icons8-launch-96.png", title: "Proyectos"},
     { iconCDN: "/icons/icons8-graduation-cap-96.png", title: "Educación"}
+  ]
+
+  externalLinks = [
+    { iconCDN: "/icons/icons8-linkedin-96.png", title: "Linkedin"},
+    { iconCDN: "/icons/icons8-github-logo-96.png", title: "GitHub"},
+    { iconCDN: "/icons/icons8-gmail-96.png", title: "Contacto"}
   ]
 }
