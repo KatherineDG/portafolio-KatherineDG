3614c7a39370ae325ae2a9d5dfd9bb9806a7dbf0
KatherineDG <katherinedomeqg@gmail.com>
2026-06-14 19:40:25 -0300

    feat: Added project images

--- src/app/pages/projects/project/project.ts
+++ src/app/pages/projects/project/project.ts
@@ -12,7 +12,9 @@ export class Project {
 
     projects = [
     { name: 'EduConnectAI',
-      images: ['educonnectai'],
+      imageFolder: 'educonnectai',
+      totalImages: 15,
+      media: ['png','png','png','png','png','png','png','png','png','png','png','png','png','png','png'],
       technologies: ['React', 'Next.js', 'TypeScript', 'HTML', 'CSS', 'Python', 'LangChain', 'LLaMa 3.1', 'ChromaDB', 'PostgreSQL', 'MongoDB', 'Docker Compose', 'EC2 (AWS)'],
       information: 'Plataforma de aprendizaje basada en IA Generativa que utiliza la técnica RAG (Retrieval-Augmented Generation). Docentes pueden entrenar sus propios agentes con contenido y estilo específico para ofrecer respuestas precisas, evitar "alucinaciones" y fomentar el pensamiento crítico en el estudiante. El ecosistema fue construido sobre una infraestructura escalable basada en microservicios compuesta por 5 módulos independientes: Billetera virtual, Sistema RAG, Base de usuarios y Chat bidireccional.',
       repositories: [
@@ -32,7 +34,9 @@ export class Project {
       ]
     },
     { name: 'Análisis de Dengue en CABA',
-      images: ['reportedengue'],
+      imageFolder: 'reportedengue',
+      totalImages: 3,
+      media: ['png','png','png'],
       technologies: ['Python', 'sklearn', 'pandas', 'Excel', 'Looker Studio'],
       information: 'Análisis de datos epidemiológicos del Dengue en CABA orientado a la optimización de recursos en salud pública. Se implementó un pipeline completo de Data Science, desde la limpieza (ETL) y suplantación de datos faltantes mediante datasets demográficos auxiliares, hasta la creación de modelos predictivos. Se uilizaron los algoritmos Random Forest y Regresión para identificar zonas críticas y predecir probabilidades de brotes por comuna, mes y grupo etario. El proyecto integra análisis y visualización estratégica en Looker Studio, transformando datos complejos en conclusiones accionables para mitigar la propagación del mosquito y reducir el gasto público en campañas preventivas.',
       repositories: [
@@ -52,7 +56,9 @@ export class Project {
       highlights: []
     },
     { name: 'BeluAr Gateway de Pagos',
-      images: ['gatewaydepagos'],
+      imageFolder: 'gatewaydepagos',
+      totalImages: 6,
+      media: ['png','png','png','png','mp4','mp4'],
       technologies: ['Python', 'Django', 'Event Driven Architecture', 'AWS', 'SNS', 'SQS', 'WebSocket', 'API Gateway', 'EC2', 'GitHub Actions'],
       information: 'Belu.ar es una plataforma de gestión hotelera basada en una arquitectura de microservicios orientada a eventos. Tomando el rol de Backend, implementé el Gateway de Pagos como pieza central para manejar transacciones, coordinando su interacción con los módulos de Reservas y BackOffice. Para ello se utilizó AWS SQS y SNS para desacoplar estos servicios, permitiendo que una reserva confirmada dispare eventos de pago y actualizaciones de estado en el panel administrativo de forma asincrónica. Esta infraestructura, garantiza la consistencia de datos entre los tres servicios asegurando alta disponibilidad y escalabilidad.',
       repositories: [
@@ -72,7 +78,9 @@ export class Project {
       highlights: []
     },
     { name: 'Kukit',
-      images: ['kukit'],
+      imageFolder: 'kukit',
+      totalImages: 8,
+      media: ['png','png','png','png','png','png','png','png'],
       technologies: ['User Research', 'Mapa de empatía', 'Proto-Persona', 'Persona', 'User Journey Map', 'Benchmarking', 'Arquitectura de la Información', 'Figma', 'Wireframing', 'Prototype', 'Leyes de Gestalt',  'Test de Usuario', 'Heurísticas de la Usabilidad de Nielsen'],
       information: 'Kükit es una plataforma móvil que transforma ingredientes disponibles en el hogar en recetas personalizadas para mitigar el desperdicio de alimentos. Como Investigadora UX y Figma Maker dentro del equipo, realicé la validación de hipótesis y el prototipado de alta fidelidad. Analicé 69 encuestas y entrevistas para construir Mapas de Empatía y User Journey Maps, identificando diferentes puntos de dolor. Se tradujeron estos hallazgos en un sistema de diseño en Figma, aplicando heurísticas de Nielsen para garantizar una navegación intuitiva. Logrando un diseño de producto donde la estética no es solo visual, sino una herramienta funcional que elimina la carga mental del usuario.',
       repositories: [
@@ -98,7 +106,9 @@ export class Project {
       highlights: []
     },
     { name: 'EcoSense',
-      images: ['ecosense'],
+      imageFolder: 'ecosense',
+      totalImages: 3,
+      media: ['png','png','png','png'],
       technologies: ['Python', 'Spark', 'Next.js', 'WebSocket', 'MongoDB', 'AWS', 'Kafka', 'EC2', 'Microservices', 'Event Driven Architecture', 'IOT Simulator'],
       information: 'EcoSense es una plataforma de monitoreo ambiental inteligente diseñada para optimizar el consumo de recursos en el sector agrícola multisede. Me enfoqué en el manejo de datos masivos, integrando Apache Spark para el análisis estadístico y procesamiento en tiempo real de métricas provenientes de sensores IoT (simulado). Se implementó la lógica de alarmado mediante Spark Streaming para detectar excesos de consumo de forma inmediata. Además, se desarrolló un dashboard interactivo en Next.js que consume datos vía WebSockets, transformando flujos complejos de Big Data en visualizaciones estratégicas que permiten disminuir la dependencia de auditorías externas y mejorar la sostenibilidad operativa.',
       repositories: [
@@ -122,6 +132,27 @@ export class Project {
   currentProject = computed(() => 
     this.projects.find(p => p.name === this.name())
   )
+
+  currentImage = 0;
+
+  nextImage() {
+    const project = this.currentProject();
+
+    if (!project) return;
+
+    this.currentImage =
+      (this.currentImage + 1) % project.totalImages;
+  }
+
+  previousImage() {
+    const project = this.currentProject();
+
+    if (!project) return;
+
+    this.currentImage =
+      (this.currentImage - 1 + project.totalImages) %
+      project.totalImages;
+  }
 
 
 
